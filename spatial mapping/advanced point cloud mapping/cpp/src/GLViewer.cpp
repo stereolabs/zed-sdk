123e6f155cee05675b87e5fa5d0a622c6db6fba7
pyver <pierre.yver@stereolabs.com>
2021-03-29 18:20:05 +0200

    [skip ci] fix point cloud mapping sample with ZED2i

--- spatial mapping/advanced point cloud mapping/cpp/src/GLViewer.cpp
+++ spatial mapping/advanced point cloud mapping/cpp/src/GLViewer.cpp
@@ -108,17 +108,19 @@ sl::CameraParameters param, sl::FusedPointCloud* ptr, sl::MODEL zed_model) {
 
     zedPath.setDrawingType(GL_LINE_STRIP);
     zedModel.setDrawingType(GL_TRIANGLES);
-    Model3D *model;
+    Model3D *model = nullptr;
     switch (zed_model)
     {
     case sl::MODEL::ZED: model = new Model3D_ZED(); break;
+    case sl::MODEL::ZED2i: 
     case sl::MODEL::ZED2: model = new Model3D_ZED2(); break;
     case sl::MODEL::ZED_M: model = new Model3D_ZED_M(); break;
     }
-    for (auto it : model->part)
-        fillZED(it.nb_triangles, model->vertices, it.triangles, it.color, &zedModel);
-    delete model;
-
+    if(model) {
+        for (auto it : model->part)
+            fillZED(it.nb_triangles, model->vertices, it.triangles, it.color, &zedModel);
+        delete model;
+    }
     zedModel.pushToGPU();
     updateZEDposition = false;
 
