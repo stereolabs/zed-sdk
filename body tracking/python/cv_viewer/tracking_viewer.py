f1d04958fa3c5719ac0ef05faffbb36bee50762b
Quy Thao TRUONG <quythao.truong@stereolabs.com>
2021-04-20 10:20:17 +0200

    Fix SK 2D display when fitting is off - python

--- body tracking/python/cv_viewer/tracking_viewer.py
+++ body tracking/python/cv_viewer/tracking_viewer.py
@@ -34,25 +34,30 @@ def render_2D(left_display, img_scale, objects, is_tracking_on):
                     kp_b = cvt(obj.keypoint_2d[part[1].value], img_scale)
                     # Check that the keypoints are inside the image
                     if(kp_a[0] < left_display.shape[1] and kp_a[1] < left_display.shape[0] 
-                    and kp_b[0] < left_display.shape[1] and kp_b[1] < left_display.shape[0]):
+                    and kp_b[0] < left_display.shape[1] and kp_b[1] < left_display.shape[0]
+                    and kp_a[0] > 0 and kp_a[1] > 0 and kp_b[0] > 0 and kp_b[1] > 0 ):
                         cv2.line(left_display, (int(kp_a[0]), int(kp_a[1])), (int(kp_b[0]), int(kp_b[1])), color, 1, cv2.LINE_AA)
                 
                 # Get spine base coordinates to create backbone
-                spine = (obj.keypoint_2d[sl.BODY_PARTS.LEFT_HIP.value] + obj.keypoint_2d[sl.BODY_PARTS.RIGHT_HIP.value]) / 2
-                kp_a = cvt(spine, img_scale)
-                kp_b = cvt(obj.keypoint_2d[sl.BODY_PARTS.NECK.value], img_scale)
+                left_hip = obj.keypoint_2d[sl.BODY_PARTS.LEFT_HIP.value]
+                right_hip = obj.keypoint_2d[sl.BODY_PARTS.RIGHT_HIP.value]
+                spine = (left_hip + right_hip) / 2
+                kp_spine = cvt(spine, img_scale)
+                kp_neck = cvt(obj.keypoint_2d[sl.BODY_PARTS.NECK.value], img_scale)
                 # Check that the keypoints are inside the image
-                if(kp_a[0] < left_display.shape[1] and kp_a[1] < left_display.shape[0] 
-                and kp_b[0] < left_display.shape[1] and kp_b[1] < left_display.shape[0]):
-                    cv2.line(left_display, (int(kp_a[0]), int(kp_a[1])), (int(kp_b[0]), int(kp_b[1])), color, 1, cv2.LINE_AA)
+                if(kp_spine[0] < left_display.shape[1] and kp_spine[1] < left_display.shape[0] 
+                and kp_neck[0] < left_display.shape[1] and kp_neck[1] < left_display.shape[0]
+                and kp_spine[0] > 0 and kp_spine[1] > 0 and kp_neck[0] > 0 and kp_neck[1] > 0
+                and left_hip[0] > 0 and left_hip[1] > 0 and right_hip[0] > 0 and right_hip[1] > 0 ):
+                    cv2.line(left_display, (int(kp_spine[0]), int(kp_spine[1])), (int(kp_neck[0]), int(kp_neck[1])), color, 1, cv2.LINE_AA)
 
                 # Skeleton joints
                 for kp in obj.keypoint_2d:
                     cv_kp = cvt(kp, img_scale)
                     if(cv_kp[0] < left_display.shape[1] and cv_kp[1] < left_display.shape[0]):
                         cv2.circle(left_display, (int(cv_kp[0]), int(cv_kp[1])), 3, color, -1)
-                cv_kp = cvt(spine, img_scale)
-                if(cv_kp[0] < left_display.shape[1] and cv_kp[1] < left_display.shape[0]):
-                    cv2.circle(left_display, (int(cv_kp[0]), int(cv_kp[1])), 3, color, -1)
+                if(kp_spine[0] < left_display.shape[1] and kp_spine[1] < left_display.shape[0]
+                and left_hip[0] > 0 and left_hip[1] > 0 and right_hip[0] > 0 and right_hip[1] > 0 ):
+                    cv2.circle(left_display, (int(kp_spine[0]), int(kp_spine[1])), 3, color, -1)
     
     cv2.addWeighted(left_display, 0.9, overlay, 0.1, 0.0, left_display)
