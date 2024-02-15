3ea488b91666ebe364d1c2500c9020e85d025b4b
Katsunori Waragai <149886409+katsunori-waragai@users.noreply.github.com>
2024-02-15 18:04:07 +0900

    Feature/bbox name (#3)
    why
    結果を画像に表示したい。
    what
    表示用の画像　image_left_ocv　に顔照合結果を描画する。

--- object detection/birds eye viewer/python/object_detection_birds_view.py
+++ object detection/birds eye viewer/python/object_detection_birds_view.py
@@ -38,19 +38,6 @@ is_jetson = False
 
 use_faceme = True
 
-if use_faceme:
-    import faceme_wrapper
-    testimg = "/home/waragai/github/ex-faceme/images/each_face/わらがい.jpg"
-    img = cv2.imread(testimg)
-    recognize_results, search_results = faceme_wrapper.process_image(img)
-    out_cvimg = img
-    out_cvimg = faceme_wrapper.draw_recognized(out_cvimg, recognize_results, search_results)
-    cv2.imwrite("out_cvimg.jpg", out_cvimg)
-    cv2.imshow("out", out_cvimg)
-    key = cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-
 if platform.uname().machine.startswith('aarch64'):
     is_jetson = True
 
@@ -182,18 +169,16 @@ def main():
                 zed.retrieve_measure(point_cloud, sl.MEASURE.XYZRGBA, sl.MEM.CPU, pc_resolution)
                 zed.get_position(cam_w_pose, sl.REFERENCE_FRAME.WORLD)
                 zed.retrieve_image(image_left, sl.VIEW.LEFT, sl.MEM.CPU, display_resolution)
+                image_render_left = image_left.get_data()
+                np.copyto(image_left_ocv,image_render_left)  # dst, src
                 if use_faceme:
                     # waragai: Here we have image_left
                     cvimage = image_left.get_data()
                     recognize_results, search_results = faceme_wrapper.process_image(cvimage)
                     summary = faceme_wrapper.bbox_and_name(recognize_results, search_results)
                     print(summary)
-                    cvimage = faceme_wrapper.draw_recognized(cvimage, recognize_results, search_results)
-                    cv2.imwrite("out_cvimg.jpg", cvimage)
-                    # cv2.imshow("out", out_cvimg)
+                    image_left_ocv = faceme_wrapper.draw_recognized(image_left_ocv, recognize_results, search_results)
 
-                image_render_left = image_left.get_data()
-                np.copyto(image_left_ocv,image_render_left)
                 track_view_generator.generate_view(objects, image_left_ocv,image_scale ,cam_w_pose, image_track_ocv, objects.is_tracked)
                 global_image = cv2.hconcat([image_left_ocv,image_track_ocv])
                 viewer.updateData(point_cloud, objects)
